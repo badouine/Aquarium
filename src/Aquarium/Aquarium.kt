d266af7fde20419d638192cc1b12a9504693a6d3
badouine <nzbadouine@gmail.com>
2022-12-27 03:52:20 +0000

    override

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -1,20 +1,29 @@
 package Aquarium
-
- class Aquarium(
+import kotlin.math.PI
+ open class Aquarium(
     var width: Int = 20,
     var height: Int = 50,
     var length: Int = 100
 ) {
 
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             height = (value * 100) / (width * length)
         }
-    var water = volume * 0.9
+    open var water = volume * 0.9
      constructor(numberOfFish: Int): this() {
         val water = numberOfFish * 2000  // cm3
          val tank = water + water * 0.1
           height = (tank / (length * width)).toInt()
      }
+}
+
+class TowerTank(): Aquarium() {
+    override var water = volume * 0.8
+    override var volume: Int
+        get() = (width * height * length / 1000 * PI).toInt()
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 }
